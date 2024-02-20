aa69f0b7b23b17c4f9586bb6b772da936bbd2288
khue-he141599 <khuendhe1415999@gmail.com>
2024-02-20 14:21:04 +0700

    update

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,15 +1,18 @@
-import styles from './Header.module.scss'; 
+import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            
-        </div>
-    </header>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
-
